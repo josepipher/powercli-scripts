57e2e9e549788040e288327441191ca3ad31d4b7
josepipher <josepipher@users.noreply.github.com>
2020-04-01 08:41:57 +0800

    update return value

--- get_datastore_consumption.ps1
+++ get_datastore_consumption.ps1
@@ -1,3 +1,7 @@
 $datastoreName = "Datastore-*"
 
-Get-Datastore | Where-Object {$_.name -Like $datastoreName } | Select Name,@{N="FreeSpaceGB";E={ [math]::Round( $_.FreeSpaceGB, 2) } },CapacityGB,@{N="Used Capacity %";E={ [math]::Round( ($_.CapacityGB - $_.FreeSpaceGB) / $_.CapacityGB * 100, 2 ) }}
+$datastoreConsumption = Get-Datastore | Where-Object {$_.name -Like $datastoreName} | Select Name,@{N="FreeSpaceGB";E={ [math]::Round( $_.FreeSpaceGB, 2) } },CapacityGB,@{N="Used Capacity %";E={ [math]::Round( ($_.CapacityGB - $_.FreeSpaceGB) / $_.CapacityGB * 100, 2 ) }} | Sort-Object Name
+
+#$datastoreConsumption
+
+return $datastoreConsumption
