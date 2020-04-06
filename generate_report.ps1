b6932cf10d5d5a8c7ddc751b8bd19bddcf691b70
josepipher <josepipher@users.noreply.github.com>
2020-04-07 03:08:50 +0800

    include emailing

--- generate_report.ps1
+++ generate_report.ps1
@@ -13,6 +13,8 @@ Write-Host ""
 Write-Host "Generating VM results ..."
 $vm_distribution = .\get_vm_perHost_or_Cluster.ps1
 
+$vcenter = ([System.Uri]$global:defaultviserver.ServiceUri.AbsoluteUri).Host
+$today = get-date -Format yyyy-MM-dd
 # Define header for table
 $Header = @"
 <title>Resource Utilization Report</title>
@@ -35,10 +37,9 @@ $host_memProvisioned_beforeHTML | ft -auto
 $ds_consumption
 $vm_distribution | ft -auto
 
-#############################################
+###########################
 # Write table to file
-# File is placed under a "Report" subfolder
-#############################################
+###########################
 $host_cpu_report = $host_cpuProvisioned_beforeHTML | ConvertTo-HTML -Fragment
 $host_mem_report = $host_memProvisioned_beforeHTML | ConvertTo-HTML -Fragment
 $ds_report = $ds_consumption | ConvertTo-HTML -Fragment
@@ -50,3 +51,16 @@ ConvertTo-Html -Body "$host_cpu_report $host_mem_report $ds_report $vm_report" -
            "Memory provisioned GB", 
            "FreeSpaceGB",
            "Total VM" -Head $Header | Out-File -FilePath $fileName
+
+#############################
+# Email report to Recipients
+#############################
+$email_sender = "Reporter <report@abc.com>"
+$email_recipient = @("ReceiverA <receiverA@abc.com>", "ReceiverB <receiverB@abc.com>")
+$smtpServer = "your-SMTP-server"
+$smtpPort = 25
+$subject = "Resource Utilization Report for $vcenter ($today)"
+$body = "Attachment from Your friend."
+Send-MailMessage -From $email_sender -To $email_recipient -Subject $subject -Body $body -SmtpServer $smtpServer -Port $smtpPort -Attachments $fileName
+
+Write-Host "Report emailed."
